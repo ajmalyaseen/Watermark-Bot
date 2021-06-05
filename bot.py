8481661cc8adbafc97ba70e52c46d4d4ca9f07ed
Diago <83084169+ajmalyaseen@users.noreply.github.com>
2021-06-05 12:44:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -578,6 +578,15 @@ async def button(bot, cmd: CallbackQuery):
 		await cmd.message.delete(True)
 		await HelpWatermark(bot, cmd.message)
 	elif cb_data.startswith("ban_"):
+        	elif cb_data == "about":
+		await cmd.message.edit(
+			text="about me??",
+			disable_web_page_preview=True,
+			reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Go Back", callback_data="goBack")]])
+		)
+	elif cb_data == "goBack":
+		await cmd.message.delete(True)
+		await HelpWatermark(bot, cmd.message)
 		if Config.UPDATES_CHANNEL is None:
 			await cmd.answer("Sorry Sir, You didn't Set any Updates Channel!", show_alert=True)
 			return
